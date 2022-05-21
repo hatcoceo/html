547344f9360233776159ec4a2159e470126e2349
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2022-05-21 09:22:26 +0700

    Create ex.php

---
+++ ex.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+	<div class="app-a">
+		<div class="header-b">header-B</div>
+		<div class="container-c">
+			<div class="sidebar-e">sidebar-e</div>
+			<div class="content-f">
+				<div class="menu-g">menu-g</div>
+				<div class="product-h">
+					<div class="pr">pr-1</div>
+					<div class="pr">pr-2</div>
+					<div class="pr">pr-3</div>
+					<div class="pr">pr-4</div>
+
+
+				</div>
+
+
+			</div>
+		</div>
+		<div class="footer-d">footer-d</div>
+	</div>
+</body>
+</html>
